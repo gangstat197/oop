78ac73a4a8905da5e4bb5332f702896828e2e7c5
Lê Đức Lưu <luusayshello1907@gmail.com>
2025-09-22 07:55:54 +0700

    feat: add 20 lines

--- HW1/Introduction.java
+++ HW1/Introduction.java
@@ -1,5 +1,12 @@
 public class Introduction {
     public static void main(String[] args) {
         System.out.println("Le Duc Luu" + '\t' + "24021557" + '\t' + "K69I-CS1" + '\t' + "gangstat197" + '\t' + "luusayshello1907@gmail.com");
+
+        for (int i = 9; i >= 1; --i) {
+            System.out.println(i + " bottles of beer on the wall, 9 bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+        }
+
+        System.out.println("No more bottles of beer on the wall.");
     }
 }
