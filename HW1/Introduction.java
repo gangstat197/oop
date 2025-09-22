368afb74c12a95dd5763964c882e9bed7c3d392d
Lê Đức Lưu <luusayshello1907@gmail.com>
2025-09-22 07:50:46 +0700

    feat: Introduction.java - B section

---
+++ HW1/Introduction.java
@@ -0,0 +1,5 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Le Duc Luu" + '\t' + "24021557" + '\t' + "K69I-CS1" + '\t' + "gangstat197" + '\t' + "luusayshello1907@gmail.com");
+    }
+}
